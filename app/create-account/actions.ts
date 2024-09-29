c180584d3cd0ff315baca8b01ce0f5eb84043ff8
0xdagarn <sgnoo@naver.com>
2024-09-29 15:02:15 +0900

    Add zod validation

---
+++ app/create-account/actions.ts
@@ -0,0 +1,58 @@
+"use server";
+import { z } from "zod";
+
+const passwordRegex = new RegExp(
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*?[#?!@$%^&*-]).+$/
+);
+
+const checkUsername = (username: string) => !username.includes("potato");
+const checkPassword = ({
+  password,
+  confirm_password,
+}: {
+  password: string;
+  confirm_password: string;
+}) => password === confirm_password;
+
+const formSchema = z
+  .object({
+    username: z
+      .string({
+        invalid_type_error: "Username must be a string!",
+        required_error: "Where is my username???",
+      })
+      .min(3, "Way too short!!!")
+      .max(10, "That is too looooong!")
+      .toLowerCase()
+      .trim()
+      .transform((username) => `🔥 ${username}`)
+      .refine(checkUsername, "No potatoes allowed!"),
+    email: z.string().email().toLowerCase(),
+    password: z
+      .string()
+      .min(4)
+      .regex(
+        passwordRegex,
+        "Passwords must contain at least one UPPERCASE, lowercase, number and special characters #?!@$%^&*-"
+      ),
+    confirm_password: z.string().min(4),
+  })
+  .refine(checkPassword, {
+    path: ["confirm_password"],
+    message: "Two passwords should be equal",
+  });
+
+export async function createAccount(prevState: any, formData: FormData) {
+  const data = {
+    username: formData.get("username"),
+    email: formData.get("email"),
+    password: formData.get("password"),
+    confirm_password: formData.get("confirm_password"),
+  };
+  const result = formSchema.safeParse(data);
+  if (!result.success) {
+    return result.error.flatten();
+  } else {
+    console.log(result.data);
+  }
+}
--- app/create-account/page.tsx
+++ app/create-account/page.tsx
@@ -1,44 +1,53 @@
-import FormButton from "@/components/form-btn";
-import FormInput from "@/components/form-input";
+"use client";
+
+import Button from "@/components/button";
+import Input from "@/components/input";
 import SocialLogin from "@/components/social-login";
+import { useFormState } from "react-dom";
+import { createAccount } from "./actions";
 
 export default function CreateAccount() {
+  const [state, dispatch] = useFormState(createAccount, null);
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
         <h1 className="text-2xl">안녕하세요!</h1>
         <h2 className="text-xl">Fill in the form below to join!</h2>
       </div>
-      <form className="flex flex-col gap-3">
-        <FormInput
+      <form action={dispatch} className="flex flex-col gap-3">
+        <Input
           name="username"
           type="text"
           placeholder="Username"
           required
-          errors={[]}
+          errors={state?.fieldErrors.username}
+          minLength={3}
+          maxLength={10}
         />
-        <FormInput
+        <Input
           name="email"
           type="email"
           placeholder="Email"
           required
-          errors={[]}
+          errors={state?.fieldErrors.email}
         />
-        <FormInput
+        <Input
           name="password"
           type="password"
           placeholder="Password"
+          minLength={4}
           required
-          errors={[]}
+          errors={state?.fieldErrors.password}
         />
-        <FormInput
-          name="confirmPassword"
+        <Input
+          name="confirm_password"
           type="password"
           placeholder="Confirm Password"
           required
-          errors={[]}
+          minLength={4}
+          errors={state?.fieldErrors.confirm_password}
         />
-        <FormButton loading={false} text="Create account" />
+        <Button text="Create account" />
       </form>
       <SocialLogin />
     </div>
